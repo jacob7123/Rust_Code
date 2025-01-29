5951deb0edcd74b02505b75862338f8c892dacbe
Chingyen Huang <h39713971@gmail.com>
2025-01-28 16:45:22 -0800

    Channels

--- Concurrency/src/main.rs
+++ Concurrency/src/main.rs
@@ -1,18 +1,20 @@
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+
 fn main() {
     /*
-        Need to sleep for waiting thread working.
-     */
+       Spawn and Join
+    */
+
+    /*  Need to sleep for waiting thread working. */
     // std::thread::spawn(move ||{
     //     println!("Hello from thread!");
     // });
 
     // thread::sleep(Duration::from_secs(1));
 
-    /*
-        Using join let thread working before scope end.
-     */
+    /* Using join let thread working before out of scope. */
     // let handle = std::thread::spawn(move ||{
     //         println!("Hello from thread!");
     // });
@@ -20,23 +22,66 @@ fn main() {
 
     // println!("Hello from main!");
 
-    let v = vec![1, 2, 3];
+    // let v = vec![1, 2, 3];
     // let handle = std::thread::spawn(move || {
     //     println!("{:?}", v);
     // });
 
-    let mut thread_handle = Vec::new();
-    for i in v{
-        thread_handle.push(std::thread::spawn(move || println!("Thread {}", i)));
-    }
+    // let mut thread_handle = Vec::new();
+    // for i in v{
+    //     thread_handle.push(std::thread::spawn(move || println!("Thread {}", i)));
+    // }
 
-    println!("Main Thread!");
+    // println!("Main Thread!");
 
-    for handle in thread_handle{
-        handle.join().unwrap();
-    }
+    // for handle in thread_handle{
+    //     handle.join().unwrap();
+    // }
 
-    println!("Main Thread!");
+    // println!("Main Thread!");
 
+    /*
+       Channels
+    */
 
+    // let (transmitter, receiver) = mpsc::channel();
+    // let tx = transmitter.clone();
+
+    // let val = String::from("Transmitting!!!!!!!!!!!!");
+    // std::thread::spawn(move ||{
+    //     transmitter.send(val).unwrap();
+    // });
+    // let msg = receiver.recv().unwrap();
+    // println!("{}", msg); // val ownership moves to msg.
+
+    // std::thread::spawn(move || {
+    //     let vec = vec![
+    //         String::from("Transmitting"),
+    //         String::from("From"),
+    //         String::from("Original"),
+    //     ];
+    //     for val in vec {
+    //         transmitter.send(val).unwrap();
+    //     }
+    // });
+
+    // std::thread::spawn(move || {
+    //     let vec = vec![
+    //         String::from("Clone"),
+    //         String::from("Is"),
+    //         String::from("Transmitting"),
+    //     ];
+    //     for val in vec {
+    //         tx.send(val).unwrap();
+    //     }
+    // });
+
+    // for rec in receiver {
+    //     println!("{}", rec);
+    // }
+
+    /*
+        Send and Sync
+     */
+    
 }
